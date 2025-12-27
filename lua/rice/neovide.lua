8d8b78526865f25bac7ce453de579732d51b5743
almrk <199596498+almrk@users.noreply.github.com>
2025-12-27 15:00:49 +0000

    Add my Neovide configuration

---
+++ lua/rice/neovide.lua
@@ -0,0 +1,8 @@
+if vim.g.neovide then
+    vim.o.guifont = "Iosevka Nerd Font Mono:h12"
+    vim.g.neovide_cursor_animation_length = 0.125
+    vim.g.neovide_cursor_short_animation_length = 0.03
+    vim.g.neovide_cursor_vfx_mode = "pixiedust"
+    vim.g.neovide_cursor_vfx_lifetime = 1.0
+    vim.g.neovide_cursor_vfx_particle_density = 25.0
+end
